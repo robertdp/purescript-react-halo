61bee58f04fd800eb209b615c5c23d6d21ef52e7
Robert Porter <robert.porter@rakuten.com>
2021-01-15 21:45:50 +0900

    Re-export Component from react-basic-hooks

--- src/React/Halo.purs
+++ src/React/Halo.purs
@@ -14,7 +14,7 @@ import Control.Parallel.Class (parallel, sequential) as Class
 import Data.Tuple.Nested (type (/\), (/\)) as TupleNested
 import Effect.Aff.Class (liftAff) as Class
 import Effect.Class (liftEffect) as Class
-import React.Basic.Hooks (JSX) as React
+import React.Basic.Hooks (Component, JSX) as React
 import React.Halo.Component (ComponentSpec, HookSpec, UseHalo, component, component_, useHalo) as Halo
 import React.Halo.Internal.Control (HaloAp, HaloM, fork, hoist, hoistAp, kill, props, subscribe, subscribe', unsubscribe) as Halo
 import React.Halo.Internal.Eval (EvalSpec, defaultEval, mkEval) as Halo
