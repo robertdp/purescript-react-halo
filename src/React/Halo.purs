fd08868e383970a6fcc66214a76656bb0648ed95
Robert Porter <robertp.dev@gmail.com>
2021-03-08 19:20:42 +0900

    Add props getter back to the top-level exports

--- src/React/Halo.purs
+++ src/React/Halo.purs
@@ -3,6 +3,6 @@ module React.Halo
   ) where
 
 import React.Halo.Component (ComponentSpec, HookSpec, UseHalo, component, useHalo) as Halo
-import React.Halo.Internal.Control (HaloAp, HaloM, fork, hoist, hoistAp, kill, context, subscribe, subscribe', unsubscribe) as Halo
+import React.Halo.Internal.Control (HaloAp, HaloM, fork, hoist, hoistAp, kill, props, context, subscribe, subscribe', unsubscribe) as Halo
 import React.Halo.Internal.Eval (EvalSpec, defaultEval, mkEval) as Halo
 import React.Halo.Internal.Types (ForkId, Lifecycle(..), SubscriptionId) as Halo
